78bcd44593b504c4575cb416781d022e84ad75ba
Allan Hart <allan.hart@fdmgroup.com>
2021-03-08 17:40:40 +0000

    first commit

---
+++ Palindrome/Palindrome.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Palindrome
+{
+    public class Palindrome
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger("Palindrome.cs");
+
+        public Boolean isPalindrome(String inputString)
+        {
+            String inputStringLower = inputString.ToLower();
+            String inputStringLowerNoSpace = inputStringLower.Replace(" ", "");
+            StringBuilder sbnormal = new StringBuilder();
+            StringBuilder sbrev = new StringBuilder();
+            foreach (char character in inputStringLowerNoSpace)
+            {
+                sbnormal.Append(character);
+                sbrev.Insert(0, character);
+            }
+            if (sbnormal.Equals(sbrev))
+            {
+                logger.Info("Phrase \"" + inputString + "\" is a palindrome");
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ Palindrome/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using log4net.Config;
+using Palindrome;
+
+namespace Palindrome
+{
+    class Program
+    {
+        private static readonly log4net.ILog logger = log4net.LogManager.GetLogger("Program.cs");
+        static void Main(string[] args)
+        {
+            BasicConfigurator.Configure();
+            Palindrome program = new Palindrome();
+            String inputString = "do geese see god";
+            String inputString2 = "go hang a salami im a lasagna hog";
+            Boolean isPalindrome1 = program.isPalindrome(inputString);
+            Boolean isPalindrome2 = program.isPalindrome(inputString2);
+            Boolean isPalindrome3 = program.isPalindrome("sjhfj");
+
+            Console.WriteLine(isPalindrome1);
+            Console.WriteLine(isPalindrome2);
+            Console.WriteLine(isPalindrome3);
+
+        }
+
+        
+    }
+}
